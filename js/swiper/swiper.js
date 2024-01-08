054d4809be98872a20db1aa2434b2be0ff1690e9
Artem14072003 <artem_krivenko_2019@mail.ru>
2024-01-08 14:09:53 +0300

    Home page adaptivity

--- js/swiper/swiper.js
+++ js/swiper/swiper.js
@@ -5,6 +5,16 @@ const swiperThumb = new Swiper('.gift__swiper--thumb', {
     spaceBetween: 16,
     slidesPerView: 6,
     freeMode: true,
+    breakpoints: {
+        320: {
+            slidesPerView: 5,
+            spaceBetween: 12,
+        },
+        1141: {
+            spaceBetween: 16,
+            slidesPerView: 6,
+        }
+    }
 });
 
 // Swiper 2 (Main)
